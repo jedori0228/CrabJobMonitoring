314a5ee37b8698c8b5ec5c1659ad447e3829f142
Jae Sung Kim <jae.sung.kim@cern.ch>
2018-09-27 01:16:28 +0200

    add timestamp

--- make_html.py
+++ make_html.py
@@ -1,4 +1,5 @@
 from SummarizeCrabStatus import *
+import datetime
 
 JobDirs = [
 '/afs/cern.ch/work/j/jskim/SKFlatMaker/ForSubmission/CMSSW_9_4_9_cand2/src/SKFlatMaker/SKFlatMaker/script/CRAB3/v949cand2_1/2017/crab_submission_DATA/crab_projects/',
@@ -8,6 +9,28 @@ JobDirs = [
 Skeleton_path = 'Skeleton_Status.html'
 os.system('cp '+Skeleton_path+' tmp_Status.html')
 out = open('tmp_Status.html','a')
+
+JobStartTime = datetime.datetime.now()
+timestamp =  JobStartTime.strftime('%Y-%m-%d %H:%M:%S')+' CERN'
+
+print>>out,'''<body>
+
+<p class="Title"> Status of SKFlat v949cand2_1 Production </p>
+<p class="Clock">Last updated time : {0}</p>
+
+<table border = 1 align="center">
+  <tr>
+    <th>Sample</th>
+    <th>Unsubmitted</th>
+    <th>Idle</th>
+    <th>Running</th>
+    <th>Failed</th>
+    <th>Transferring</th>
+    <th>Finished</th>
+    <th>Total</th>
+    <th>%</th>
+  </tr>
+'''.format(timestamp)
 
 for JobDir in JobDirs:
 
@@ -32,4 +55,4 @@ print>>out,'''</table>
 '''
 
 out.close()
-os.system('cp tmp_Status.html Status.html')
+os.system('mv tmp_Status.html Status.html')
