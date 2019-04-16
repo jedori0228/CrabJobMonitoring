ea899c30d00affd14be4d5b43cc77aeb3e60d099
Jae Sung Kim <jae.sung.kim@cern.ch>
2019-04-16 23:17:39 +0200

    Do not reset proxy if already exist

--- setup.sh
+++ setup.sh
@@ -7,14 +7,26 @@ if [[ -z $CMSSW_BASE ]]; then
   return
 fi
 
-## Init Proxy
-echo "@@@@ Setting proxy.."
-echo "@@@@ type your grid proxy password when asked"
-voms-proxy-init --voms cms --valid 192:00
-while [[ $? == 1 ]]; do
-  printf "\n@@@@ Proxy is not set, do it again\n"
-  voms-proxy-init --voms cms --valid 192:00
-done
+## Check if proxy exist
+echo "@@@@ Checking current proxy info.."
+voms-proxy-info
+if [[ $? == 1 ]]; then
+
+	## Init Proxy
+	echo "@@@@ Setting proxy.."
+	echo "@@@@ type your grid proxy password when asked"
+	voms-proxy-init --voms cms --valid 192:00
+	while [[ $? == 1 ]]; do
+		printf "\n@@@@ Proxy is not set, do it again\n"
+		voms-proxy-init --voms cms --valid 192:00
+	done
+
+else
+
+  echo "@@@@ Proxy exists, so I skip here."
+  echo "@@@@ If you reset the proxy again here, you might have issues with another shell using same proxy"
+
+fi
 
 ## set crab
 echo "@@@@ set crab3"
